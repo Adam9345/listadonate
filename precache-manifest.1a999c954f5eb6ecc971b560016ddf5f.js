2387dd36b1a5feab71e47c944b82596ffe769362
adamklo93 <klonica3a@gmail.com>
2019-04-25 18:01:05 +0200

    Updates

---
+++ precache-manifest.1a999c954f5eb6ecc971b560016ddf5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa340fe26c28bf9ee389ba02f1b2f1c",
+    "url": "/listadonate/index.html"
+  },
+  {
+    "revision": "7e22ca1cd9e9678d3a58",
+    "url": "/listadonate/static/css/main.93cb2e23.chunk.css"
+  },
+  {
+    "revision": "e6bab553d9655d86e3ed",
+    "url": "/listadonate/static/js/2.20b3ae07.chunk.js"
+  },
+  {
+    "revision": "7e22ca1cd9e9678d3a58",
+    "url": "/listadonate/static/js/main.519c781c.chunk.js"
+  },
+  {
+    "revision": "00236ccbc2f7d8efacaf",
+    "url": "/listadonate/static/js/runtime~main.d7e8755d.js"
+  }
+]);
